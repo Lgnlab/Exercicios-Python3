7a6f282f5d4f9d55c5485bf453439972e9fe83df
Lucas Gabriel Do Nascimento <lucas.gabriel.nascimento.br@gmail.com>
2026-06-13 23:42:47 -0300

    Exercicios

--- Listas17.py
+++ Listas17.py
@@ -1,11 +1,11 @@
 def juntando(vetor_1 = 0, vetor_2 = 0):
     a = []
     b = []
-    for cont_a in range(1, 4):
+    for cont_a in range(1, 11):
         vetor_1 = int(input(f"{cont_a} Número: "))
         a.append(vetor_1)
     print("=" * 12)
-    for cont_b in range(1, 4):
+    for cont_b in range(1, 11):
         vetor_2 = int(input(f"{cont_b} Número: "))
         b.append(vetor_2)
     c = a + b
